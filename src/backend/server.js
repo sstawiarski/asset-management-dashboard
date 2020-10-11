eb066ed5ea808caa8c235be7abdbb7c87aeaf0cd
EFritts <efritts@asu.edu>
2020-10-11 14:42:04 -0500

    US-131 Updates server.js for mongoose inclusion

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://ser401:<ser401>@cluster0.bjvvr.mongodb.net/<Explore>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
 const PORT = 4000;
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:3000"
+}));
 app.use(bodyParser.json());
 app.listen(PORT, function() {
     console.log("Server is running on Port: " + PORT);
 });
 
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
+mongoose.connect("mongodb+srv://ser401:<ser401>@cluster0.bjvvr.mongodb.net/<Explore>?retryWrites=true&w=majority", {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+
+
+    
+    app.listen(PORT, function () {
+        console.log("Server is running on Port: " + PORT);
+    });
 });
