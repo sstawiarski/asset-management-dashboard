60932e1d2b4a5f678e4657bbe05975f60a5efe3d
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-10-13 13:19:23 -0400

    Define servers

---
+++ backend/documentation/servers.js
@@ -0,0 +1,12 @@
+const servers = [
+    {
+        url: 'http://localhost:4000',
+        description: 'Local server'
+    }, 
+    {
+        url: 'https://future_api_url',
+        description: 'Production server'
+    }
+]
+
+module.exports = servers;
