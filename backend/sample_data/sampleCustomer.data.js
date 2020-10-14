be0e76841281aa22980c02ef111d56fa1056ec5f
eric fritts <eric.fritts@live.com>
2020-10-13 21:17:17 -0500

    US-120 Updates JSON sample data files to single files

---
+++ backend/sample_data/sampleCustomer.data.js
@@ -0,0 +1,40 @@
+const sampleCustomer = [
+    {
+  
+        "customerId": "0981764325",
+        "firstName": "",
+        "lastName": "",
+        "companyName": "BHGE Drilling",
+        "addressLine1": "345 Main St",
+        "addressLine2": "Suite 1912",
+        "city": "Houston",
+        "state": "TX",
+        "zip": "77049"
+    },
+    {
+
+        "customerId": "09825",
+        "firstName": "John",
+        "lastName": "Martin",
+        "companyName": "JM Drilling",
+        "addressLine1": "345 Main St",
+        "addressLine2": "Suite 1909",
+        "city": "Katy",
+        "state": "TX",
+        "zip": "77084"
+    },
+    {
+
+        "customerId": "098223455",
+        "firstName": "",
+        "lastName": "",
+        "companyName": "Bob's Drilling",
+        "addressLine1": "12959 Route 32",
+        "addressLine2": "",
+        "city": "Broken Arrow",
+        "state": "OK",
+        "zip": "79958"
+    }
+];
+
+module.exports = sampleCustomer;
