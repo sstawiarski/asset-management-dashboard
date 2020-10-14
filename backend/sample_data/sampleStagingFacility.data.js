82b4d35976c5fa3834b7e4dec941c0f0db23e9e0
eric fritts <eric.fritts@live.com>
2020-10-13 23:40:38 -0500

    US-120 Updates sample data

---
+++ backend/sample_data/sampleStagingFacility.data.js
@@ -0,0 +1,28 @@
+const sampleStagingFacility = [
+    {
+
+        "facilityName": "Nabors TX yard",
+        "operator": "Nabors Drilling",
+        "rigName": "Nabors 124",
+        "contactName": "Paula Sullivan",
+        "contactNumber": "702-846-8245"
+    },
+    {
+
+        "facilityName": "Rowan OK",
+        "operator": "Rowan Energy",
+        "rigName": "",
+        "contactName": "Ray Finkle",
+        "contactNumber": "123-456-7890"
+    },
+    {
+
+        "facilityName": "The Spot",
+        "operator": "Spots, Inc",
+        "rigName": "SLB-12",
+        "contactName": "Ginger O'Hare",
+        "contactNumber": "558-925-1274"
+    }
+]
+
+module.exports = sampleStagingFacility;
