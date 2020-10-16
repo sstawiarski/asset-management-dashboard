a81d7f9056b0d6700e82858af8e6bab8a823c947
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-10-16 14:09:15 -0400

    US148-T157: Create Header component

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+
+
+const Header = ({ heading, subheading }) => {
+ return (
+        <div style={{flexGrow: 1, padding: "40px"}}>
+            <Typography variant="h2">{heading}</Typography>
+            <Divider />
+            {subheading ? <Typography variant="h4">{subheading}</Typography> : null}
+        </div>
+    );
+};
+
+export default Header;
