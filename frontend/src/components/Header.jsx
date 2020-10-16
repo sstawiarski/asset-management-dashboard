e7f8f01f3d63cf85138a1c8375b45eb2517d3df9
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-10-16 15:48:54 -0400

    Install icon packs and edit styling

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import Divider from '@material-ui/core/Divider'
 
 const Header = ({ heading, subheading }) => {
  return (
-        <div style={{flexGrow: 1, padding: "40px"}}>
+        <div style={{flexGrow: 1, padding: "40px", float: "left"}}>
             <Typography variant="h2">{heading}</Typography>
             <Divider />
             {subheading ? <Typography variant="h4">{subheading}</Typography> : null}
