f9b0dc979810fa1c47fa6c26fbc9677e02a2d571
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-10-19 14:51:20 -0400

    US4-T28: Generate event search from user input

--- backend/models/event.model.js
+++ backend/models/event.model.js
@@ -16,8 +16,18 @@ const Event = new Schema({
     productIds: { type: mongoose.Schema.Types.Array, required: true, unique: false },
     eventType: { type: String, enum: eventTypes, required: true, unique: false },
     eventTime: { type: mongoose.Schema.Types.Date, required: true, unique: false },
-    eventData: { type: mongoose.Schema.Types.Mixed, required: true, unique: false }
+    eventData: { type: mongoose.Schema.Types.Mixed, required: true, unique: false },
+    confidenceScore: { type: mongoose.Schema.Types.Number, required: false, unique: false }
 });
+
+Event.plugin(mongoose_fuzzy_searching, {
+    fields: [
+        {
+            name: 'key',
+            escapeSpecialCharacters: true
+        }
+    ]
+})
 
 const Events = mongoose.model('event', Event);
 
--- backend/routes/events.routes.js
+++ backend/routes/events.routes.js
@@ -6,6 +6,45 @@ const Event = require('../models/event.model');
 const Shipment = require('../models/shipment.model')
 const sampleEvents = require('../sample_data/sampleEvents.data')
 
+router.get('/', async (req, res) => {
+    try {
+        if (req.query.search) {
+            const search = req.query.search.replace("-", "");
+            const events = await Event.fuzzySearch(search).limit(5);
+            
+            if (events.length) {
+                if (events[0].key.toUpperCase() === req.query.search) {
+                    const result = [events[0]];
+                    res.status(200).json(result);
+                }
+                else {
+                    if (assets[0].confidenceScore > 10) {
+                        const result = events.filter(item => item.confidenceScore > 10);
+                        res.status(200).json(result)
+                    } else {
+                        res.status(200).json(events)
+                    }
+                }
+            }
+        } else {
+            const events = await Event.find({});
+            if (assets) res.status(200).json(assets);
+            else res.status(500).json({
+                message: "No events found in database",
+                interalCode: "no_events_found"
+            })
+        }
+        
+    }
+    catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Error searching for events in database",
+            internal_code: "event_search_error"
+        })
+    }
+})
+
 router.put('/load', (req, res) => {
     try {
         sampleEvents.forEach(async (item) => {
