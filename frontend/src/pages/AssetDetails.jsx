e983bf339de17b1b01f41568226ffa67fda38770
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-10-22 18:15:57 -0400

    Fix messed up Material-UI Grid import

--- frontend/src/pages/AssetDetails.jsx
+++ frontend/src/pages/AssetDetails.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles'
 
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import Divider from '@material-ui/core/Typography';
 import Header from '../components/Header'
 import AssetTimeline from '../components/AssetTimeline'
