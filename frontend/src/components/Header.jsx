d9db70b56953df26073dba13021e80bf8721da83
Eric Fritts <eric.fritts@live.com>
2020-10-23 12:48:29 -0500

    US68 updates generic tests for travis

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-  
-import React from 'react';
-
-import Typography from '@material-ui/core/Typography'
-import Divider from '@material-ui/core/Divider'
-
-
-const Header = ({ heading, subheading }) => {
- return (
-        <div style={{flexGrow: 1, padding: "40px", float: "left"}}>
-            <Typography variant="h2">{heading}</Typography>
-            <Divider />
-            {subheading ? <Typography variant="h4">{subheading}</Typography> : null}
-=======
 import React from 'react';
 
 import Typography from '@material-ui/core/Typography'
@@ -24,7 +9,6 @@ const Header = ({ heading, subheading }) => {
             <Typography variant="h3">{heading}</Typography>
             <hr style={{flexBasis: "100%"}} />
             {subheading ? <Typography variant="h5">{subheading}</Typography> : null}
->>>>>>> development
         </div>
     );
 };
