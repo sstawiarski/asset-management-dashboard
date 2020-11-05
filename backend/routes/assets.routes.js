a72bb8faa475b8d8362a581e41698541ecff1394
Shawn Stawiarski <contact@shawnstawiarski.com>
2020-11-05 18:17:18 -0500

    US5-T196: Implement server-side sorting and paging
    for assets

--- backend/routes/assets.routes.js
+++ backend/routes/assets.routes.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const { GridFSBucketReadStream } = require("mongodb");
 const router = express.Router();
 const mongoose = require("mongoose");
 const { nGrams } = require('mongoose-fuzzy-searching/helpers');
-const { searchFilter } = require("../documentation/schemas");
 const connection = mongoose.connection;
 const Asset = require("../models/asset.model");
 const sampleAssets = require("../sample_data/sampleAssets.data");
@@ -14,31 +12,76 @@ router.get("/", async (req, res, err) => {
 
     let aggregateArray = [];
 
-    if (req.query.search) {
-      const searchTerm = req.query.search.replace("-", "");
-      const search = {
-        $match: {
-          $text: {
-            $search: nGrams(searchTerm, null, false).join(' ')
+    //if there are possible filters other than search
+    if (req.query) {
+      const query = req.query;
+
+      //remove page, limit, search, and sorting params since they do not go in the $match
+      const disallowed = ["page", "limit", "search", "sort_by", "order"];
+      const filters = Object.keys(query)
+        .reduce((p, c) => {
+
+          /* MongoDB compares exact dates and times
+          If we want to see an entire 24 hours, we much get the start and end times of the given day
+          And get everything in between the start and end */
+          if (c === "dateCreated" || c === "dateUpdated") {
+            p[c] = {
+              $gte: new Date(parseInt(query[c])).setHours(0, 0, 0, 0),
+              $lte: new Date(parseInt(query[c])).setHours(23, 59, 59, 999)
+            };
+          } else if (!disallowed.includes(c)) {
+            //convert the "true" and "false" strings in the query into actual booleans
+            if (query[c] === "true") {
+              p[c] = true;
+            } else if (query[c] === "false") {
+              p[c] = false;
+            } else {
+              p[c] = query[c];
+            }
+          };
+          return p;
+        }, {});
+
+      if (req.query.search) {
+        const searchTerm = req.query.search.replace("-", "");
+
+        //match search and regular filters
+        const search = {
+          $match: {
+            $text: {
+              $search: nGrams(searchTerm, null, false).join(' ')
+            },
+            ...filters
           }
         }
-      }
 
-      const confidenceScore = {
-        $addFields: {
-          confidenceScore: { $meta: "textScore" }
+        const confidenceScore = {
+          $addFields: {
+            confidenceScore: { $meta: "textScore" }
+          }
         }
-      }
 
-      aggregateArray.push(search);
-      aggregateArray.push(confidenceScore)
+        aggregateArray.push(search);
+        aggregateArray.push(confidenceScore)
+
+      } else {
+
+        //match only the regular filters
+        const match = {
+          $match: {
+            ...filters
+          }
+        };
+        aggregateArray.push(match);
+      }
 
     } else {
       const match = {
         $match: {
-
+          
         }
       };
+
       aggregateArray.push(match);
     }
 
@@ -47,49 +90,96 @@ router.get("/", async (req, res, err) => {
       const sortOrder = (req.query.order === 'desc' ? -1 : 1);
 
       if (req.query.search) {
-        const sort = { 
-          $sort: { 
+        const sort = {
+          $sort: {
             confidenceScore: -1,
-            [req.query.sort_by]: sortOrder 
-          } 
+            [req.query.sort_by]: sortOrder
+          }
         };
         aggregateArray.push(sort);
 
       } else {
-        const sort = { 
-          $sort: { 
-            [req.query.sort_by]: sortOrder 
-          } 
+        const sort = {
+          $sort: {
+            [req.query.sort_by]: sortOrder
+          }
         };
         aggregateArray.push(sort);
       }
     }
 
+    //pagination initial setup
+    const skip = {
+      $skip: (req.query.page && req.query.limit) ? (parseInt(req.query.page) * parseInt(req.query.limit)) : 0
+    }
+    //aggregateArray.push(skip);
+
     //limit to 5 results -- modify later based on pagination
-    const limit = { 
-      $limit: 5 
+    const limit = {
+      $limit: req.query.limit ? parseInt(req.query.limit) : 5
     };
-    aggregateArray.push(limit);
+    //aggregateArray.push(limit);
+
+    const group = {
+      $facet: {
+        count: [{ $count: "count" }],
+        data: [skip, limit]
+      }
+    };
+    aggregateArray.push(group);
 
     //remove irrelevant fields from retrieved objects
     const projection = {
       $project: {
         _id: false,
-        __v: false,
-        serial_fuzzy: false
+        'data._id': false,
+        'data.__v': false,
+        'data.serial_fuzzy': false
       }
     }
     aggregateArray.push(projection);
 
     const result = await Asset.aggregate(aggregateArray);
 
-    if (result) {
-      res.status(200).json(result);
+    //filter results to determine better or even exact matches
+    if (req.query.search) {
+
+      if (result.length) {
+
+        if (result[0].data[0].serial.toUpperCase() === req.query.search.toUpperCase()) {
+          const exactMatch = [result[0].data[0]];
+          res.status(200).json(exactMatch);
+
+        } else {
+
+          if (result[0].data[0].confidenceScore > 10) {
+            const closeMatches = result[0].data.filter(asset => asset.confidenceScore > 10);
+            res.status(200).json(closeMatches);
+
+          } else {
+            res.status(200).json(result[0]);
+          }
+        }
+
+      } else {
+        res.status(404).json({
+          message: "No assets found in database",
+          interalCode: "no_assets_found",
+        });
+      }
+
+      //return all results found if not a search
     } else {
-      res.status(404).json({
-        message: "No assets found in database",
-        interalCode: "no_assets_found",
-      });
+
+      if (result.length) {
+        res.status(200).json(result[0]);
+
+      } else {
+        res.status(404).json({
+          message: "No assets found in database",
+          interalCode: "no_assets_found",
+        });
+      }
     }
 
   } catch (err) {
@@ -142,29 +232,5 @@ router.get("/:serial", async (req, res, err) => {
     });
   }
 });
-
-router.get("/searchFilter", async (req, res, err) => {
-  const filter = req.params.search;
-  try {
-    const asset = await Asset.find({ searchFilter: searchFilter });
-
-    if (asset.length) {
-      res.status(200).json(asset[0]);
-    } else {
-      res.status(500).json({
-        message: "No assets found for serial",
-        internalCode: "no_assets_found",
-      });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({
-      message: "serial is missing",
-      interalCode: "missing_parameters",
-    });
-  }
-});
-
-
 
 module.exports = router;
