ba1ac84720d5b96ce404b041c67e9538564c93a5
Eric Fritts <eric.fritts@live.com>
2020-11-13 15:32:58 -0600

    US-10 Adds subtance to CreateAssetDialog

---
+++ frontend/src/components/Dialogs/CreateAssetDialog.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+import Button from '@material-ui/core/Button';
+import Select from '@material-ui/core/Select';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    item: {
+        paddingTop: theme.spacing(2)
+    },
+    formControl: {
+        margin: theme.spacing(1),
+        minWidth: 180,
+    },
+    error: {
+        color: "red"
+    }
+}));
+
+const CreateAssetDialog = ({ open, setOpen, selected }) => {
+    const classes = useStyles();
+
+    /* Store state of select dropdown */
+    const [status, setStatus] = useState("");
+    const [failed, setFailed] = useState(null);
+
+    /* Helper method to send update command -- uses async so we can use 'await' keyword */
+    const sendData = async (data) => {
+
+        //uses PATCH endpoint and sends the arguments in the body of the HTTP request
+        const result = await fetch("http://localhost:4000/assets", {
+            method: "POST",
+            mode: 'cors',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        return result;
+    }
+
+    
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        //setup data object to send based on API docs and required parameters
+        const data = {
+            assets: selected,
+            update: {
+                retired: status === "Active" ? false : true
+            }
+        }
+
+        sendData(data)
+        .then(response => {
+
+            //assume anything less than 300 is a success
+            if (response.status < 300) {
+                return response.json();
+            } else return null;
+        })
+        .then(json => {
+
+            //check if we got back null and either close dialog on success or setFailed to render error message
+            if (json) {
+                handleClose();
+            } else {
+                setFailed(true);
+            }
+        })
+    }
+
+    //reset dialog to default state on close
+    const handleClose = () => {
+        setOpen(false);
+        setStatus("");
+        setFailed(null);
+    }
+
+    return (
+        <Dialog open={open} onClose={handleClose} aria-labelledby="create-asset-dialog-title">
+            
+            <DialogTitle id="create-asset-dialog-title">Create Asset</DialogTitle>
+            
+            <DialogContent>
+                
+                
+                <div className={classes.item}>
+                    <FormControl variant="outlined" className={classes.formControl}>
+                        <InputLabel id="asset-serial-label">Asset Serial</InputLabel>
+
+                        {/* Controlled select, get value from state and changes state when it changes */}
+                        <Select
+                            labelId="asset-serial-label"
+                            labelWidth={105}
+                            id="asset-serial-select"
+                            value={status}
+                            onChange={(event) => setStatus(event.target.value)}
+                        >
+
+                            /* populate menu items here for available serials */
+
+                        </Select>
+
+                        {/* Render a failure message if API returns a response code > 300 */}
+                        {failed ? <Typography variant="subtitle1" className={classes.error}>Error submitting change</Typography> : null}
+                    </FormControl>
+                </div>
+            </DialogContent>
+
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Cancel
+                </Button>
+                <Button onClick={handleSubmit} type="submit" color="primary">
+                    Submit
+                </Button>
+            </DialogActions>
+
+        </Dialog>
+    );
+};
+
+export default CreateAssetDialog;
