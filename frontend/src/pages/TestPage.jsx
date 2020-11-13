fd870a8bdde52a57c47917351c25cf98650d82d0
Eric Fritts <eric.fritts@live.com>
2020-11-13 15:37:16 -0600

    US-10 adds test page for visual and troubleshooting

---
+++ frontend/src/pages/TestPage.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+import Button from '@material-ui/core/Button';
+
+import CreateAssetDialog from '../components/CreateAssetDialog';
+
+const TestPage = () => {
+
+    {/* Control open/close of retire dialog */}
+    const [createOpen, setCreateOpen] = useState(false);
+
+    return (
+        <div>
+            <Button onClick={() => setCreateOpen(!retireOpen)} variant="contained" color="primary">Create Asset</Button>
+            
+            {/* Take in open and assets from parent component so dialog can know what is selected */}
+            <CreateAssetDialog open={createOpen} setOpen={setCreateOpen} assets={['G800-1119']} />
+        </div>
+    );
+};
+
+export default TestPage;
