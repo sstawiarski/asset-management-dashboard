e654cc631e1d5dce95db63e08c4f7b7354d67b32
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-01-19 19:47:53 -0500

    US10-T211: Implement asset creation frontend

--- frontend/src/components/Dialogs/CreateAssetDialog.jsx
+++ frontend/src/components/Dialogs/CreateAssetDialog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -12,90 +12,172 @@ import InputLabel from '@material-ui/core/InputLabel';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormLabel from '@material-ui/core/FormLabel';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
     item: {
-        paddingTop: theme.spacing(2)
     },
     formControl: {
-        margin: theme.spacing(1),
+        marginBottom: theme.spacing(2),
         minWidth: 180,
+        marginLeft: "20px"
     },
-    selectEmpty: {
-        marginTop: theme.spacing(2)
+    entry: {
+        marginLeft: "20px"
     },
     error: {
-        color: "red"
-    }
+
+    },
+    range: {
+        marginLeft: "20px",
+        width: "40%"
+    },
+    owner: {
+        marginBottom: theme.spacing(2),
+        minWidth: 240,
+        marginLeft: "20px",
+        marginTop: theme.spacing(4)
+    },
 }));
 
-const CreateAssetDialog = ({ open, setOpen, handleCreate, selected }) => {
+const CreateAssetDialog = ({ open, setOpen, onSuccess, onSemiSuccess }) => {
     const classes = useStyles();
 
-    /* Store state of select dropdown */
     const [status, setStatus] = useState("");
     const [failed, setFailed] = useState("");
-    const [assetName, setAssetName] = useState("");
-    const [serial, setSerial] = useState("");
+    const [options, setOptions] = useState([]);
+    const [serials, setSerials] = useState("");
+    const [schema, setSchema] = useState("");
+    const [incorrect, setIncorrect] = useState([]);
+    const [entryType, setEntryType] = useState("range");
+    const [beginRange, setBeginRange] = useState("");
+    const [endRange, setEndRange] = useState("");
     const [owner, setOwner] = useState("");
-    const [assignmentType, setAssignmentType] = useState("");
-    const [groupTag, setGroupTag] = useState("");
-    const [assignee, setAssignee] = useState("");
-    const [asset, getAsset] = useState({});
-    const data = {
-        assetName: assetName,
-        serial: serial,
-        owner: owner,
-        assignmentType: assignmentType,
-        groupTag: groupTag,
-        assignee: assignee
-        
-    }
-    
+    const [serialEntered, setEntered] = useState(true);
+    const [missingType, setMissingType] = useState(false);
+    const [missingBegin, setMissingBegin] = useState(false);
+    const [missingEnd, setMissingEnd] = useState(false);
 
-    /* Helper method to send update command -- uses async so we can use 'await' keyword */
-    
+    /* Fetches list of asset types for dropdown */
+    useEffect(() => {
+        fetch('http://localhost:4000/assets/schemas')
+            .then(res => res.json())
+            .then(json => setOptions(json));
+    }, [])
 
-  
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        //setup data object to send based on API docs and required parameters
-        
+        if (schema === "") {
+            setMissingType(true);
+            return;
+        }
 
-        sendData(data)
-        .then(response => {
+        if (entryType === "range" && (beginRange === "" || endRange === "")) {
+            if (beginRange === "") setMissingBegin(true);
+            if (endRange === "") setMissingEnd(true);
+            return;
+        }
 
-            //assume anything less than 300 is a success
-            if (response.status < 300) {
-                return response.json();
-            } else return null;
-        })
-        .then(json => {
+        /* setup data object to send based on API docs and required parameters */
+        let data = {};
 
-            //check if we got back null and either close dialog on success or setFailed to render error message
-            if (json) {
-                handleClose();
-            } else {
-                setFailed(true);
+        if (entryType === "list") {
+            if (serials === "") {
+                setEntered(false);
+                return;
             }
-        })
-        //console.log(data);
+            //split up serials entered in text box
+            const serial = serials.split('\n');
+            if (serial[serial.length - 1] === '') serial.pop();
+
+            //check serials to see if they match schema
+            const check = options.filter(item => item.name === schema);
+            const serialTest = check[0].serializationFormat.split("-");
+            const badOnes = serial.filter(s => {
+                const item = s.split('-');
+                return item[0] !== serialTest[0];
+            });
+
+            const realBad = badOnes.map((item, idx) => {
+                if (item === "") {
+                    return {};
+                } else {
+                    return {
+                        serial: item,
+                        line: serial.indexOf(item) + 1
+                    }
+                }
+            })
+
+            if (realBad.length > 0) {
+                if (Object.keys(realBad[0]).length === 0) {
+                    setIncorrect([]);
+                } else {
+                    setIncorrect(realBad);
+                    return;
+                }
+            } else {
+                setIncorrect([]);
+                data.type = entryType;
+                data.list = serial;
+                data.assetName = schema;
+                data.owner = owner;
+                sendData(data).then(res => {
+                    if (res.invalid.length > 0) {
+                        onSemiSuccess(res.invalid);
+                    } else {
+                        onSuccess(true, res.message);
+                    }
+                    handleClose();
+                });
+            }
+        } else if (entryType === "range") {
+            data.type = "range";
+            data.serialBase = options.find(element => element.name === schema).serializationFormat;
+            data.beginRange = beginRange;
+            data.endRange = endRange;
+            data.owner = owner;
+            data.assetName = schema;
+            sendData(data).then(res => {
+                if (res.invalid.length > 0) {
+                    onSemiSuccess(res.invalid);
+                } else {
+                    onSuccess(true, res.message);
+                }
+            });
+            handleClose();
+        }
+    }
+
+    const handleEntryTypeChange = (event) => {
+        setEntryType(event.target.value);
+        setSerials("");
+        setBeginRange("");
+        setEndRange("");
+        setIncorrect([]);
+        setMissingBegin(false);
+        setMissingEnd(false);
+        setMissingType(false);
+        setEntered(true);
     }
 
     const sendData = async (data) => {
-
         //uses POST endpoint and sends the arguments in the body of the HTTP request
-        const result = await fetch("http://localhost:4000/assets/create-Asset", {
+        const result = await fetch("http://localhost:4000/assets", {
             method: "POST",
             mode: 'cors',
             headers: {
                 'Content-Type': 'application/json'
-              },
+            },
             body: JSON.stringify(data)
         });
-        return result;
+        return await result.json();
     }
 
     //reset dialog to default state on close
@@ -103,120 +185,132 @@ const CreateAssetDialog = ({ open, setOpen, handleCreate, selected }) => {
         setOpen(false);
         setStatus("");
         setFailed(null);
-       
+        setSerials("");
+        setSchema("");
+        setBeginRange("");
+        setEndRange("");
+        setEntered(true);
+        setMissingBegin(false);
+        setMissingEnd(false);
+        setMissingType(false);
+        setOwner("");
+        setEntryType("range");
+        setIncorrect([]);
     }
 
-   
+    const re = /^[0-9\b]+$/;
+
 
     return (
-        <Dialog open={open} onClose={handleClose} aria-labelledby="create-asset-dialog-title">
-            
-            
-          
-            <DialogTitle id="create-asset-dialog-title">Create Asset</DialogTitle>
-            
+        <Dialog maxWidth="xs" fullWidth open={open} onClose={handleClose} aria-labelledby="create-asset-dialog-title">
+
+
+
+            <DialogTitle id="create-asset-dialog-title">Create Assets</DialogTitle>
+
             <DialogContent>
-                
-                
+
+
                 <div className={classes.item}>
                     <FormControl variant="outlined" className={classes.formControl}>
                         <InputLabel id="asset-type-label">Asset Name</InputLabel>
-                        
+
                         <Select
                             labelId="asset-type-label"
                             labelWidth={105}
                             id="asset-type-label"
-                            value={assetName}
-                            onChange={(event) => setAssetName(event.target.value)}
+                            error={missingType}
+                            value={schema}
+                            onChange={(event) => { 
+                                if (missingType) setMissingType(false);
+                                setSchema(event.target.value) 
+                            }}
                         >
-                            <MenuItem value="Centralizer">Centralizer</MenuItem>
-                            <MenuItem value="Gap Sub">Gap Sub</MenuItem>
-                            <MenuItem value="Crossover Sub">Crossover Sub</MenuItem>
-                            <MenuItem value="Carrier">Carrier</MenuItem>
+                            {options ? options.map((option, key) => <MenuItem key={key} value={option.name}>{option.name}</MenuItem>) : null}
 
                         </Select>
-   
                     </FormControl>
-                
-                    <FormControl variant="outlined" className={classes.formControl}>
-                        <InputLabel id="asset-serial-label">Asset Serial</InputLabel>
-                            <Select
-                                labelId="asset-serial-label"
-                                labelWidth={105}
-                                id="asset-serial-label"
-                                value={serial}
-                                onChange={(event) => setSerial(event.target.value)}
-                            >
-                            </Select>
-         
+                    <FormControl component="fieldset" className={classes.entry}>
+                        <FormLabel component="legend">Method</FormLabel>
+                        <RadioGroup row aria-label="method" name="method" value={entryType} onChange={handleEntryTypeChange}>
+                            <FormControlLabel value="range" control={<Radio />} label="Range" />
+                            <FormControlLabel value="list" control={<Radio />} label="List" />
+                        </RadioGroup>
                     </FormControl>
+                    <br />
+                    {entryType === "list"
+                        ?
+                        <div className={classes.entry}>
+                            <TextField
+                                id="serial-list"
+                                style={{ width: "100%" }}
+                                multiline
+                                rows={6}
+                                variant="outlined"
+                                error={!serialEntered}
+                                value={serials}
+                                onChange={(event) => { 
+                                    if (!serialEntered) setEntered(true);
+                                    setSerials(event.target.value) 
+                                }}
+                                onKeyDown={(event) => {
+                                    if (event.key === "Enter") {
+                                        console.log(serials)
+                                    }
+                                }}>
 
-                    <FormControl variant="outlined" className={classes.formControl}>
-                        <InputLabel id="asset-owner-label">Asset Owner</InputLabel>
-                        
+                            </TextField>
+                            {incorrect.length > 0 ?
+                                <>
+                                    <Typography variant="subtitle1" style={{ color: "red" }}><b>ERROR: Some serials are incorrectly formatted:</b></Typography>
+                                    <ul>
+                                        {incorrect.map(item => <><li key={item.serial}>{item.serial} (line {item.line})</li></>)}
+                                    </ul>
+                                </>
+                                : null}
+                        </div>
+                        :
+                        <div>
+                            <TextField
+                                className={classes.range}
+                                variant="outlined"
+                                value={beginRange}
+                                onChange={(event) => { 
+                                    if (missingBegin) setMissingBegin(false);
+                                    setBeginRange(event.target.value) 
+                                }}
+                                error={re.test(beginRange) ? false : beginRange !== "" ? true : missingBegin ? true : false}
+                            />
+                            <span style={{ display: "inline-block", padding: "15px 0px 15px 15px", marginRight: "-5px" }}> to </span>
+                            <TextField
+                                className={classes.range}
+                                variant="outlined"
+                                value={endRange}
+                                onChange={(event) => { 
+                                    if (missingEnd) setMissingEnd(false);
+                                    setEndRange(event.target.value) 
+                                }}
+                                error={re.test(endRange) ? false : endRange !== "" ? true : missingEnd ? true : false}
+                            />
+                        </div>
+                    }
+
+                    <FormControl variant="outlined" className={classes.owner}>
+                        <InputLabel id="asset-type-label">Owner</InputLabel>
                         <Select
-                            labelId="asset-owner-label"
+                            labelId="owner-label"
                             labelWidth={105}
-                            id="asset-owner-label"
+                            fullWidth
+                            id="owner-label"
                             value={owner}
+                            defaultValue="Supply Chain-USA"
                             onChange={(event) => setOwner(event.target.value)}
                         >
-                            <MenuItem value="Evolution-USA">Evolution-USA</MenuItem>
-                            <MenuItem value="Evolution-Calgary">Evolution-Calgary</MenuItem>
+                            {/* TODO: De-hardcode */}
+                            <MenuItem value="Supply Chain-USA">Supply Chain-USA</MenuItem>
+                            <MenuItem value="Supply Chain-CA">Supply Chain-CA</MenuItem>
 
                         </Select>
-         
-                    </FormControl>
-
-                    <FormControl variant="outlined" className={classes.formControl}>
-                        <InputLabel id="assignee-label">Assignee</InputLabel>
-                        
-                        <Select
-                            labelId="assignee-label"
-                            labelWidth={105}
-                            id="assignee-label"
-                            value={assignee}
-                            onChange={(event) => setAssignee(event.target.value)}
-                        >
-                            <MenuItem value="Nabors">Nabors</MenuItem>
-                            <MenuItem value="BHGE">BHGE</MenuItem>
-
-                        </Select>
-         
-                    </FormControl>
-
-                    <FormControl variant="outlined" className={classes.formControl}>
-                        <InputLabel id="assignment-type-label">Assignment Type</InputLabel>
-                        
-                        <Select
-                            labelId="assignment-type-label"
-                            labelWidth={105}
-                            id="assignment-type-label"
-                            value={assignmentType}
-                            onChange={(event) => setAssignmentType(event.target.value)}
-
-                        >
-                            <MenuItem value="Owned">Owned</MenuItem>
-                            <MenuItem value="Rental">Rental</MenuItem>
-
-                        </Select>
-         
-                    </FormControl>
-
-                    <FormControl variant="outlined" className={classes.formControl}>
-                        
-                        <TextField
-                            
-                            margin="dense"
-                            id="group-tag-label"
-                            label="Group Tag"
-                            type="text"
-                            fullWidth
-                            onChange={(event) => setGroupTag(event.target.value)}
-                        >
-
-                        </TextField>
-         
                     </FormControl>
                 </div>
             </DialogContent>
