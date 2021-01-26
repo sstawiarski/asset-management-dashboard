9bcdc3a5ddd487ffa59de360ecf45b5300dfa0c3
Eric Fritts <eric.fritts@live.com>
2021-01-25 21:31:12 -0600

    US-274 T-275: Updates manifest model with updated manifest information

--- backend/models/manifest.model.js
+++ backend/models/manifest.model.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const Manifest = new Schema({
+const ManifestItems = new Schema({
     item: { type: String, required: true, unique: false },
     type: { type: String, required: true, unique: false },
     quantity: { type: Number, required: true, unique: false },
     notes: { type: String, required: false, unique: false }
 });
 
-module.exports = Manifest;
+const Manifest = new Schema({
+    createdBy: { type: String, required: false, unique: false },
+    date: { type: mongoose.Schema.Types.Date, required: false, unique: false },
+    manifest: [ManifestItems]
+});
+
+const Manifests = mongoose.model('manifest', Manifest);
+
+module.exports = Manifests;
