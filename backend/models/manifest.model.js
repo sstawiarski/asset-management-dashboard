a6e9b9d04b77eadc3be1a532e818d41034bd8f31
Eric Fritts <eric.fritts@live.com>
2021-02-06 16:25:52 -0600

    US-274 Updates manifest.model to remove date

--- backend/models/manifest.model.js
+++ backend/models/manifest.model.js
@@ -1,18 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const ManifestItems = new Schema({
+const Manifest = new Schema({
     item: { type: String, required: true, unique: false },
     type: { type: String, required: true, unique: false },
     quantity: { type: Number, required: true, unique: false },
     notes: { type: String, required: false, unique: false }
 });
 
-const Manifest = new Schema({
-    date: { type: mongoose.Schema.Types.Date, required: false, unique: false },
-    manifest: [ManifestItems]
-});
+// const Manifest = new Schema({
+//     date: { type: mongoose.Schema.Types.Date, required: false, unique: false },
+//     manifest: [ManifestItems]
+// });
 
-//const Manifests = mongoose.model('manifest', Manifest);
+const Manifests = mongoose.model('manifest', Manifest);
 
 module.exports = Manifest;
