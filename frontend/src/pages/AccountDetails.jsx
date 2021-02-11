3cfa7f7f676fcd1ad3d7d804eb1f195f56254093
makingst <makingst@asu.edu>
2021-02-10 21:51:12 -0600

    add fields and sample data

--- frontend/src/pages/AccountDetails.jsx
+++ frontend/src/pages/AccountDetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory, useParams } from 'react-router-dom';
-
+import { makeStyles } from '@material-ui/core/styles'
 import TextField from '@material-ui/core/TextField';
 import Box from '@material-ui/core/Box';
 import Tooltip from '@material-ui/core/Tooltip';
@@ -12,18 +12,90 @@ import Header from '../components/Header';
 import CustomTable from '../components/Tables/CustomTable'
 import TableToolbar from '../components/Tables/TableToolbar';
 import ChipBar from '../components/Tables/ChipBar';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const assetFields = ["serial", "assetName", "assetType", "owner", "checkedOut", "groupTag"];
 const eventFields = ["key", "eventTime", "eventType"];
+const employeeID = "123456";
+const username = "jsmith";
+const email = "johnsmith@gmail.com";
+const password = "*******";
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        marginLeft: "10px",
+    },
+    paper: {
+        width: "100%",
+    },
+    item: {
+        padding: "10px",
+
+    },
+    center: {
+        display: "block",
+        marginLeft: "auto",
+        marginRight: "auto",
+        width: "50%"
+    },
+    break: {
+        flexGrow: 1,
+        fontWeight: "bold"
+    }
+   
+}));
 const AccountDetails = () => {
     
-
+	const classes = useStyles();
 
     return (
         <div>
-            <Header heading="John Smith" subheading="Account Details" />
-           
+            <Header heading="John Smith" />
+
+            <Grid container>
+            	
+            	<Grid item xs={3} className={classes.item}>
+                     <Typography variant="subtitle1" className={classes.break}>Employee ID:</Typography>
+                </Grid>
+                <Grid item xs={3} className={classes.item}>
+                	<Typography variant="body1">{employeeID}</Typography>
+                </Grid>
+                      
+            </Grid >
+
+            <Grid container>
+            	
+            	<Grid item xs={3} className={classes.item}>
+                     <Typography variant="subtitle1" className={classes.break}>Username:</Typography>
+                </Grid>
+                <Grid item xs={3} className={classes.item}>
+                	<Typography variant="body1">{username}</Typography>
+                </Grid>
+                      
+            </Grid >
+
+            <Grid container>
+            	
+            	<Grid item xs={3} className={classes.item}>
+                     <Typography variant="subtitle1" className={classes.break}>Email:</Typography>
+                </Grid>
+                <Grid item xs={3} className={classes.item}>
+                	<Typography variant="body1">{email}</Typography>
+                </Grid>
+                      
+            </Grid >
+
+            <Grid container>
+            	
+            	<Grid item xs={3} className={classes.item}>
+                     <Typography variant="subtitle1" className={classes.break}>Password:</Typography>
+                </Grid>
+                <Grid item xs={3} className={classes.item}>
+                	<Typography variant="body1">{password}</Typography>
+                </Grid>
+                      
+            </Grid >
 
 
       
