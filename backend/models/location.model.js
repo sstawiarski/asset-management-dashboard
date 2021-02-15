c9ee6e68025e9cc0bc19e53ccff954720cdd6e19
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-02-15 17:31:55 -0500

    US296-T314: Create new Location model

---
+++ backend/models/location.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const locationTypes = [
+    "Rig",
+    "Repair Facility",
+    "Staging Facility"
+];
+
+const Location = new Schema({
+    key: { type: String, required: true, unique: true },
+    locationName: { type: String, required: true, unique: false },
+    locationType: { type: String, enum: locationTypes, required: true, unique: false },
+    client: { type: String, required: false, unique: false },
+    operator: { type: String, required: false, unique: false },
+    address: { type: String, required: false, unique: false },
+    city: { type: String, required: false, unique: false },
+    state: { type: String, required: true, unique: false },
+    zip: { type: String, required: false, unique: false },
+    contactName: { type: String, required: false, unique: false },
+    contactNumber: { type: String, required: true, unique: false }
+});
+
+const Locations = mongoose.model('location', Location);
+
+module.exports = Locations;
