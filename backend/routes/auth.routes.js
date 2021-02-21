6f66bbad81b9d8cc099d2335f66f6ee63e431521
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-02-20 19:05:09 -0500

    US9: Implement endpoint to fetch user info

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -5,6 +5,7 @@ const connection = mongoose.connection;
 const Employee = require('../models/employee.model');
 const encrypt = require('../auth.utils').encrypt;
 const decrypt = require('../auth.utils').decrypt;
+const base64url = require('base64url');
 
 /* 
  * Simple POST endpoint for user authentication
@@ -49,16 +50,25 @@ router.post('/login', async (req, res) => {
     }
 });
 
-router.get('/auth/:userId', (request, response) => {
+router.get('/:user', async (req, res) => {
+    try {
+        const user = base64url.decode(req.params.user);
+        const employee = decrypt(JSON.parse(user).uniqueId);
+        const { employeeId } = JSON.parse(employee);
 
-    const accountId = Number(request.params.id);
-    const getAccount = accounts.find((account) => account.id === accountId);
-  
-    if (!getAccount) {
-      response.status(500).send('Account not found.')
-    } else {
-      response.json(getAccount);
+        let found = await Employee.findOne({ employeeId: employeeId }, { __v: 0, _id: 0 });
+        if (found) {
+            const passwordLength = found.password.length;
+            found.set('password', undefined, { strict: false });
+            found.set('passwordLength', passwordLength, { strict: false });
+            res.status(200).json(found);
+        } else {
+            res.status(404).json({ message: "Unable to find user", internalCode: "user_not_found" });
+        }
+    } catch (err) {
+        res.status(500);
+        console.log(err);
     }
-  });
+});
 
 module.exports = router;
