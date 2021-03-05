2d1f086d0f351339beba34b625f5e7207503205a
Eric Fritts <eric.fritts@live.com>
2021-03-04 21:01:55 -0600

    US-317 Adds additional sample shipment data for loading to Mongo

--- backend/sample_data/sampleShipment.data.js
+++ backend/sample_data/sampleShipment.data.js
@@ -25,7 +25,143 @@ const sampleShipment = [
             }
         ],
         "key": "SHIP-100"
+    },
+    {
+        "createdBy": "Test 2",
+        "created": Date.now(),
+        "updated": Date.now(),
+        "completed": Date.now(),
+        "status": "Staging",
+        "shipmentType": "Incoming",
+        "specialInstructions": "seriously, don't lose this",
+        "contractId": "345678",
+        "manifest": [
+            {
+                "item": "X800-87650",
+                "type": "Crossover Sub",
+                "quantity": 1,
+                "notes": "just testing this beast out!",
+                "serialized": true
+            },
+            {
+                "item": "box of boxes of batteries",
+                "type": "batteries",
+                "quantity": 1500,
+                "notes": "keep batteries in individual cases!  fire hazard!  there's a lot of 'em!",
+                "serialized": false
+            }
+        ],
+        "key": "SHIP-101"
+    },
+    {
+        "createdBy": "Test 3",
+        "created": Date.now(),
+        "updated": Date.now(),
+        "completed": Date.now(),
+        "status": "Staging",
+        "shipmentType": "Incoming",
+        "specialInstructions": "seriously, don't lose this",
+        "contractId": "345678",
+        "manifest": [
+            {
+                "item": "X800-87650",
+                "type": "Crossover Sub",
+                "quantity": 1,
+                "notes": "just testing this beast out!",
+                "serialized": true
+            },
+            {
+                "item": "box of boxes of batteries",
+                "type": "batteries",
+                "quantity": 1500,
+                "notes": "keep batteries in individual cases!  fire hazard!  there's a lot of 'em!",
+                "serialized": false
+            }
+        ],
+        "key": "SHIP-102"
+    },
+    { 
+        "createdBy": "Test 4",
+        "created": Date.now(),
+        "updated": Date.now(),
+        "completed": Date.now(),
+        "status": "Staging",
+        "shipmentType": "Incoming",
+        "specialInstructions": "seriously, don't lose this",
+        "contractId": "345678",
+        "manifest": [
+            {
+                "item": "X800-87650",
+                "type": "Crossover Sub",
+                "quantity": 1,
+                "notes": "just testing this beast out!",
+                "serialized": true
+            },
+            {
+                "item": "box of boxes of batteries",
+                "type": "batteries",
+                "quantity": 1500,
+                "notes": "keep batteries in individual cases!  fire hazard!  there's a lot of 'em!",
+                "serialized": false
+            }
+        ],
+        "key": "SHIP-103"
+    },
+    {
+        "createdBy": "Test 5",
+        "created": Date.now(),
+        "updated": Date.now(),
+        "completed": Date.now(),
+        "status": "Staging",
+        "shipmentType": "Incoming",
+        "specialInstructions": "seriously, don't lose this",
+        "contractId": "345678",
+        "manifest": [
+            {
+                "item": "X800-87650",
+                "type": "Crossover Sub",
+                "quantity": 1,
+                "notes": "just testing this beast out!",
+                "serialized": true
+            },
+            {
+                "item": "box of boxes of batteries",
+                "type": "batteries",
+                "quantity": 1500,
+                "notes": "keep batteries in individual cases!  fire hazard!  there's a lot of 'em!",
+                "serialized": false
+            }
+        ],
+        "key": "SHIP-104"
+    },
+    {
+        "createdBy": "Test 6",
+        "created": Date.now(),
+        "updated": Date.now(),
+        "completed": Date.now(),
+        "status": "Staging",
+        "shipmentType": "Incoming",
+        "specialInstructions": "seriously, don't lose this",
+        "contractId": "345678",
+        "manifest": [
+            {
+                "item": "X800-87650",
+                "type": "Crossover Sub",
+                "quantity": 1,
+                "notes": "just testing this beast out!",
+                "serialized": true
+            },
+            {
+                "item": "box of boxes of batteries",
+                "type": "batteries",
+                "quantity": 1500,
+                "notes": "keep batteries in individual cases!  fire hazard!  there's a lot of 'em!",
+                "serialized": false
+            }
+        ],
+        "key": "SHIP-105"
     }
+
 ];
 
 module.exports = sampleShipment;
