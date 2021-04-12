5df4d4b313cfe59b87d340c24097c8ca1d1bdbae
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-04-12 13:19:10 -0400

    US406: TypeScript setup and general fixes

--- frontend/src/components/General/Header.jsx
+++ frontend/src/components/General/Header.jsx
@@ -4,7 +4,7 @@ import Typography from '@material-ui/core/Typography'
 import Divider from '@material-ui/core/Divider';
 
 
-const Header = ({ heading, subheading }) => {
+const Header = ({ heading, subheading = "" }) => {
     return (
         <div className="header" style={{ display: "flex", flexFlow: "wrap", marginLeft: "10px", marginBottom: "20px", marginRight: "7vh" }}>
             <Typography variant="h4">{heading}</Typography>
