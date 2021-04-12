4f9068883eb7cf1b2c173ce1c10720bb7512567b
Shawn Stawiarski <contact@shawnstawiarski.com>
2021-04-12 18:04:36 -0400

    US254-T292: Fix sidebar formatting and onClick

--- frontend/src/components/General/Sidebar.jsx
+++ frontend/src/components/General/Sidebar.jsx
@@ -46,6 +46,7 @@ const useStyles = makeStyles((theme) => ({
         color: "white",
         textShadow: "1px 1px 4px #0f0f0f",
         flexBasis: "100%",
+        cursor: "pointer"
     },
     drawer: {
         width: drawerWidth,
@@ -76,8 +77,6 @@ const useStyles = makeStyles((theme) => ({
         alignItems: 'center',
         justifyContent: 'flex-end',
         padding: theme.spacing(1, 0, 0, 0),
-        // necessary for content to be below app bar
-        ...theme.mixins.toolbar,
         flexWrap: "wrap",
         backgroundColor: theme.palette.primary.main
     },
@@ -181,13 +180,17 @@ const isHighlighted = (location, link, open) => {
         } else if (text === "/shipments/create" && link === "Add New" && open) {
             return true;
         } else if (text === "/account" && link === "Account" && !open) {
-                return true; 
+            return true;
         } else {
             return false;
         }
     }
     return false;
 }
+
+const assetPages = ['View All', 'Assembly Manager'];
+const shipmentPages = ['View All', 'Add New'];
+const sidebarSections = ['Home', 'Assets', 'Shipments', 'Sign Out'];
 
 const Sidebar = ({ location }) => {
     const classes = useStyles();
@@ -246,8 +249,7 @@ const Sidebar = ({ location }) => {
 
                     {/* Account icon, name, and settings header section */}
                     <List className={classes.accountHeader}>
-                        <ListItem button={!open} style={open ? { marginLeft: "-20px" } : null} disableRipple={!open ? true : false} onClick={() => history.push('/account')}>
-
+                        <ListItem button={!open} style={open ? { marginLeft: "-20px" } : null} disableRipple={!open ? true : false}>
                             {/* Render account icon as one of 2 types of buttons for better styling open vs. closed */}
                             {
                                 open ?
@@ -255,35 +257,38 @@ const Sidebar = ({ location }) => {
                                         size="small"
                                         disableRipple
                                         style={{ marginLeft: "20px", marginRight: "10px" }}
+                                        onClick={() => history.push('/account')}
                                     >
                                         <AccountCircleIcon />
                                     </IconButton>
                                     :
-                                    <ListItemIcon>
-                                        <AccountCircleIcon 
-                                        style={{ marginLeft: "24px" }} 
-                                        className={isHighlighted(location, "Account", open) ?
-                                            !open ? classes.highlighted : classes.moduleText
-                                            : classes.nonHighlighted}
+                                    <ListItemIcon onClick={() => history.push('/account')}>
+                                        <AccountCircleIcon
+                                            style={{ marginLeft: "24px" }}
+                                            className={isHighlighted(location, "Account", open) ?
+                                                !open ? classes.highlighted : classes.moduleText
+                                                : classes.nonHighlighted}
                                         />
                                     </ListItemIcon>
                             }
 
-
-
                             {/* Logged in user name */}
                             <ListItemText
                                 primary={fullName}
+                                onClick={() => history.push('/account')}
                                 className={classes.name}
+                                title="Account details"
                                 primaryTypographyProps={{ variant: "h6", style: !open ? { width: 0, visibility: "hidden" } : null }}
                             />
+
 
                             {/* Settings icon */}
                             <IconButton
                                 size="small"
                                 disableRipple
+                                disabled={true}
                                 style={!open ? { width: 0, visibility: "hidden" } : null}
-                                onClick={(e) => { 
+                                onClick={(e) => {
                                     e.stopPropagation();
                                     history.push('/settings');
                                 }}
@@ -304,7 +309,7 @@ const Sidebar = ({ location }) => {
                 {/* Generate list of actual sidebar links */}
                 <List>
                     {
-                        ['Home', 'Assets', 'Shipments', 'Sign Out'].map(text => {
+                        sidebarSections.map(text => {
                             return (
                                 <React.Fragment key={text}>
                                     <ListItem
@@ -381,7 +386,7 @@ const Sidebar = ({ location }) => {
                                                 <List component="div" disablePadding>
                                                     {
                                                         open ?
-                                                            ["View All", "Assembly Manager", "Assignments"].map((item, idx) => {
+                                                            assetPages.map((item, idx) => {
                                                                 return (
                                                                     <ListItem
                                                                         key={idx}
@@ -435,7 +440,7 @@ const Sidebar = ({ location }) => {
                                                     <List component="div" disablePadding>
                                                         {
                                                             open ?
-                                                                ["View All", "Track", "Add New"].map((item, idx) => {
+                                                                shipmentPages.map((item, idx) => {
                                                                     return (
                                                                         <ListItem
                                                                             key={idx}
